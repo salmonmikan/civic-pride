511cd226130fd396dc4e18aac5edf60a72f45f58
Yu yume <yuki2001yuki@gmail.com>
2023-05-04 04:28:46 +0900

    push

---
+++ ga.js
@@ -0,0 +1,9 @@
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-TVH6JHNM6Y"></script>');
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-TVH6JHNM6Y');
