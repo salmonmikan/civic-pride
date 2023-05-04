429d3cd2708a5dadb2c5cda48bcded809c85d0d9
Yu yume <yuki2001yuki@gmail.com>
2023-05-04 19:37:12 +0900

    push

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// グラフを描画する関数
 function drawChart(voteData) {
   const data = {
     labels: voteData.labels,
@@ -35,7 +36,62 @@ function drawChart(voteData) {
   });
 }
 
+// デフォルトの投票データ
+const defaultVoteData = {
+  labels: ['店舗A', '店舗B', '店舗C', '店舗D', '店舗E'],
+  data: [0, 0, 0, 0, 0],
+};
+
+// 投票データをlocalStorageから取得する関数
+function getVoteData() {
+  const storedData = localStorage.getItem('voteData');
+  return storedData ? JSON.parse(storedData) : defaultVoteData;
+}
+
+// 投票データをlocalStorageに保存する関数
+function saveVoteData(voteData) {
+  localStorage.setItem('voteData', JSON.stringify(voteData));
+}
+
+// URLパラメーターから店舗名を取得、投票する関数
+function castVote(storeParam) {
+  const storeIndex = voteData.labels.indexOf(storeParam);
+
+  if (storeIndex !== -1) {
+    voteData.data[storeIndex]++;
+    saveVoteData(voteData);
+    console.log(`投票が完了しました: ${storeParam}`);
+  } else {
+    console.log('無効な店舗パラメーター');
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
+  const scriptTag = document.querySelector('script[src="main.js"]');
+  const page = scriptTag.dataset.page;
+
+  switch (page) {
+    case 'index':
+      initIndexPage();
+      break;
+    case 'vote':
+      initVotePage();
+      break;
+    default:
+      console.error('Unknown page type:', page);
+  }
+});
+
+function initIndexPage() {
+  // index.htmlで実行する処理を記述
   const voteData = getVoteData();
   drawChart(voteData);
-});
+}
+
+function initVotePage() {
+  // vote.htmlで実行する処理を記述
+  const voteData = getVoteData();
+
+}
+
